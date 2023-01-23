3bc87740eba75d51634690e9670a9898aa0e9239
YASMINE <y_habi@estin.dz>
2023-01-23 18:17:27 +0100

    add delete edit

--- frontend/src/Components/Customer.js
+++ frontend/src/Components/Customer.js
@@ -1,5 +1,45 @@
-function Customer(){
-    return <h3>HELLO!</h3>
+import { useState, useEffect } from "react";
+import { useParams,Link, useNavigate } from "react-router-dom";
+import AddCustomer from "./AddCustomer";
+
+export default function Customer(){
+    const [customers, setCustomers] = useState();
+    const navigate = useNavigate();
+    
+    
+    const navigateToContacts = () => {
+        // 👇️ navigate to /contacts
+        navigate('/EditUser');
+      };
+    useEffect(() =>{
+        console.log('Fetching...');
+        fetch('http://127.0.0.1:8000/api/customers/')
+        .then((response) =>response.json())
+        .then((data) =>{
+            console.log(data);
+            setCustomers(data.customers);
+        });
+    } ,[]);
+   return (
+    <>
+    <h1>Here are our customers:</h1>
+    {customers
+        ? customers.map((customer) => {
+              return (
+                  <div className="m-2" key={customer.id}>
+                      <Link to={'/EditCustomer/' + customer.id}>
+                          <button className="no-underline bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded">
+                              {customer.name}
+                          </button>
+                      </Link>
+                  </div>
+              );
+          })
+        : null}
+
+
+</>
+   );
+    
 }
 
-export default Customer;
